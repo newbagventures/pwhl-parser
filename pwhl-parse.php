d64e0ec1f699e88c16f988ad262d20f01f29c525
Wesley Furgiuele <wesley@newbagventures.com>
2025-01-03 15:35:18 -0500

    tweaking debugging output

--- pwhl-parse.php
+++ pwhl-parse.php
@@ -19,13 +19,13 @@ while( ( $line = fgetcsv( $fh ) ) !== FALSE ) {
         for( $i = 0; $i < $total_elements_in_details; $i++ ) {
             echo "<p>We are on key $i<br />\n";
             if( substr_count( strtolower( $line_array[$i] ), "material" ) > 0 ) {
-                echo "<strong>Found Material Details</strong>: " . $line_array[$i];
+                echo "<strong>Found Material Details</strong>: " . str_replace( "<p>", "", str_replace( "</p>", "", $line_array[$i] ) );
             }
             if( substr_count( strtolower( $line_array[$i] ), "care" ) > 0 ) {
-                echo "<strong>Found Care Instructions</strong>: " . $line_array[$i];
+                echo "<strong>Found Care Instructions</strong>: " . str_replace( "<p>", "", str_replace( "</p>", "", $line_array[$i] ) );
             }
             if( substr_count( strtolower( $line_array[$i] ), "fit" ) > 0 ) {
-                echo "<strong>Found Fit Info</strong>: " . $line_array[$i];
+                echo "<strong>Found Fit Info</strong>: " . str_replace( "<p>", "", str_replace( "</p>", "", $line_array[$i] ) );
             }
             echo "</p>\n";
         }
